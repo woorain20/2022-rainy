143a671945d12301480903a3185d7ec927d74416
ArcheTree <kimplp2002@naver.com>
2022-05-04 12:39:42 +0900

    5/4오전 모음

---
+++ TeamPJ/RoomIf/DoubleRoom.java
@@ -0,0 +1,33 @@
+package RoomIf;
+
+
+
+public class DoubleRoom {
+		
+public static void main(String[] args) {
+	String room = "Double Room";  // 방이름
+	int phoneNum = 0;    //폰번호<--view에서 가져오기
+	int date = 0;       //이용기간
+	String name = "";  //이름 
+	int cost = 7000; //비용
+
+	
+		System.out.println("\t\t 예약을 도와드리겠습니다.\t");
+		System.out.println("연락처 : "+phoneNum);
+		System.out.println("예약자 : "+name);
+		System.out.println("결제 상품 : "+room);
+		System.out.println("이용기간 : "+date);
+	
+		// 혜택 조식, 미니바 헬스자이
+
+	
+		
+
+		
+			
+
+		}
+
+
+
+}
---
+++ TeamPJ/View/HotelView.java
@@ -0,0 +1,65 @@
+package View;
+
+import java.util.Scanner;
+
+public class HotelView {
+	
+	private Scanner sc=new Scanner(System.in);
+	
+	int n;
+	
+	public void Reservation() {
+		
+		//예약 화면
+		System.out.println("Welcome to NWJ Hotel");
+		System.out.println();
+		System.out.println("아래의 항목 중 원하시는 항목의 번호를 선택해주세요");
+		System.out.println("======================================================");
+		System.out.println("1. 예약 || 2. 예약조회 || 3. 예약변경 || 4. 예약취소 || 5. 종료");
+		System.out.println("Reservation: ");
+		System.out.println();
+		
+		n=sc.nextInt();
+		
+		switch(n) {
+		
+		case 1:
+			System.out.println("예약을 선택하셨습니다.");
+			//HotelReservation 가져오기
+			HotelReservation hv=new HotelReservation();
+			hv.Room();
+			hv.Night();
+			hv.CheckIn();
+			break;
+			
+		case 2:
+			System.out.println("예약조회를 선택하셨습니다.");
+			
+			break;
+			
+		case 3:
+			System.out.println("예약을 변경하겠습니다.");
+			
+			break;
+			
+		case 4:
+			System.out.println("예약을 취소하겠습니다.");
+			
+			break;
+			
+		default:
+			System.out.println("종료");
+		}	
+		
+	}
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+			String name=sc.nextLine();
+			System.out.println("예약자명 : "+name);
+			System.out.println("연락처(-없이 입력해주세요)");
+			int phoneN=sc.nextInt();
+		HotelView res=new HotelView();
+		
+		res.Reservation();
+	}
+}
