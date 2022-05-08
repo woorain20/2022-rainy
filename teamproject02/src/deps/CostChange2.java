1287a2807b49753815aad86d0e54fa35223993ea
ArcheTree <104178714+ArcheTree@users.noreply.github.com>
2022-05-09 07:04:46 +0900

    cost 마지막 수정재료

--- teamproject02/src/deps/CostChange2.java
+++ teamproject02/src/deps/CostChange2.java
@@ -9,6 +9,7 @@ public class CostChange2 {
 	//요일 숫자 변환 
 		
 	public static double cost=10000;  //임시지정 < 가격은 다른 곳에서 가져오도록 하기
+	public static double tocost=0;
 
 	//주말 추가요금	
 	public static void plusecost(LocalDate date) {
@@ -38,8 +39,8 @@ public class CostChange2 {
 		for(int i=0; i<4;i++) {
 		bookday=bookday.plusDays(i);
 		plusecost(bookday);
-//			cost+=cost;
-		System.out.println("결제금 : "+(int) cost+"원");
+			tocost+=cost;
+
 
 		}
 		return cost;
@@ -47,6 +48,7 @@ public class CostChange2 {
 
 	public static void main(String[] args) {  //메일 말고 다른거해서 넘어가도록 지정
 		totalCost();
+		System.out.println("결제금 : "+(int) tocost+"원");
 //		for(int n=0; n<2 ;n++) {
 //			
 //			System.out.println(n);
