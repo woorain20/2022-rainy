e1b09b2d79b6f416eb70471de95e1204cf15ea43
woorain20 <superwgzi@hanmail.net>
2022-08-03 16:19:26 +0900

    museumlist

--- SpringProject/museum/src/App.svelte
+++ SpringProject/museum/src/App.svelte
@@ -1,6 +1,5 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
+  import MuseumInfo from './lib/MuseumInfo.svelte'
 </script>
 
 <main>
@@ -29,26 +28,9 @@
       </div>
     </div>
   <div>
-    <a href="https://vitejs.dev" target="_blank"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
 </main>
+<div>
+    <MuseumInfo/>
+</div>
 
 
---
+++ SpringProject/museum/src/lib/museumList/CentralMuseum.svelte
@@ -0,0 +1,65 @@
+<script>
+    import src from "../infoimg/centralmuesum1.jpg"
+    
+</script>
+
+<body>
+<div id="museum">
+    <div id="central">
+        <h2>국립 중앙박물관</h2>
+        <div>
+            <img {src} alt="국립중앙박물관">
+        </div>
+        <div>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2660.8965812951724!2d126.97676049742918!3d37.52307752115598!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357ca2208965526d%3A0xd9a7a08c18440ed4!2z6rWt66a97KSR7JWZ67CV66y86rSA!5e0!3m2!1sko!2skr!4v1659507635480!5m2!1sko!2skr"
+             width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <table>
+                <tr>
+                    <td>주소</td>
+                    <td>서울 용산구 서빙고로 137 (용산동6가, 국립중앙박물관)</td>
+                </tr>
+                <tr>
+                    <td>전화번호</td>
+                    <td>02-2077-9000</td>
+                </tr>
+                <tr>
+                    <td>개관</td>
+                    <td>1945년 12월 3일</td>
+                </tr>
+                <tr>
+                    <td>이용시간</td>
+                    <td>
+                        월/화/목/금요일 : 10:00-18:00
+                        수/토요일 : 10:00-21:00
+                        일/공휴일 : 10:00-18:00
+                    </td>
+                </tr>
+                <tr>
+                    <td>휴무일</td>
+                    <td>1월1일, 설날, 추석 4월과 11월 첫째 월요일</td>
+                </tr>
+                <tr>
+                    <td>요금</td>
+                    <td>무료</td>
+                </tr>
+                <tr>
+                    <td>편의시설</td>
+                    <td>
+                        청각장애인 수어상담
+                        시각장애인 정자 안내판
+                        엘리베이터
+                        장애인 화장실
+                        장애인 주차장
+                        임산부 및 유아휴게공간
+                    </td>
+                </tr>
+                <tr>
+                    <td>홈페이지</td>
+                    <td><a href="https://www.museum.go.kr" target="_blank">국립중앙박물관 바로가기</a></td>
+                </tr>
+            </table>
+        </div>
+    </div>
+</div>
+
+</body>
