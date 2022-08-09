a3a9f23a6088134fabed4f2d6e4a089eaad2e745
ArcheTree <kimplp2002@naver.com>
2022-08-09 12:26:46 +0900

    app update

--- SpringProject/museum/src/App.svelte
+++ SpringProject/museum/src/App.svelte
@@ -25,47 +25,10 @@
   <header id="header" class="d-flex align-items-center">
     <div class="container d-flex justify-content-between align-items-center">
 
-<<<<<<< Updated upstream
       <div class="logo">
         <h1><a href="index.html">Korea Muesum</a></h1>
         <!-- Uncomment below if you prefer to use an image logo -->
         <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
-=======
-  //import Exhibitiontotal from "./lib/Exhibitiontotal.svelte";
-
-  // import Relic from "./lib/relic/Relic.svelte";
-  // import Eventpage from "./lib/Eventpage.svelte";
-  // import Upsidposter from "./lib/mainpage/Upsidposter.svelte";
-  import Tesr from "./lib/relic/tesr.svelte"; 
-
-
-
-
-</script>
-
-<main>
-    <div class="navbar navbar-dark bg-dark fixed-top navbar-expand-lg">
-      <div class="container">
-        <a class="navbar-brand" href="../index.html">Korea Museum</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-content">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbar-content">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item, nav-link">박물관 정보</li>
-            <li class="nav-item, nav-link">국내 유물</li>
-            <li class="nav-item, nav-link">전시회</li>
-            <li class="nav-item, nav-link">EVENT</li>
-            <li class="nav-item, nav-link">게시판</li>
-            <li class="nav-item, nav-link">Museum Shop</li>
-            <li class="nav-item, nav-link">About Us</li>
-          </ul>
-          <ul class="navbar-nav">
-            <li class="nav-item, nav-link" href="#">Log In</li>
-            <li class="nav-item, nav-link" href="#">회원가입</li>
-          </ul>
-        </div>
->>>>>>> Stashed changes
       </div>
 
       <nav id="navbar" class="navbar">
@@ -88,15 +51,7 @@
 
 <Upsidposter />
 
-<<<<<<< Updated upstream
 <style>
-=======
-</div>
-
-<!-- <Exhibitiontotal /> -->
-<!-- <Relic/> -->
-<Tesr/>
->>>>>>> Stashed changes
 
 
 </style>
