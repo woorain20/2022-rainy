9d7d50daf69880e232377b2f75815982bbe29e0a
ArcheTree <kimplp2002@naver.com>
2022-08-09 12:20:48 +0900

    8/9 1

--- SpringProject/museum/src/App.svelte
+++ SpringProject/museum/src/App.svelte
@@ -1,57 +1,57 @@
 <script>
+  import Upsidposter from "./lib/mainpage/Upsidposter.svelte";
+  
+  
+  
+  </script>
 
-<<<<<<< Updated upstream
-//import Exhibitiontotal from "./lib/Exhibitiontotal.svelte";
-
-import Tesr from "./lib/relic/tesr.svelte";
-
-  // import Relic from "./lib/relic/Relic.svelte";
-=======
-import Eventpage from "./lib/Eventpage.svelte";
-import Exhibitiontotal from "./lib/Exhibitiontotal.svelte";
-import Upsidposter from "./lib/mainpage/Upsidposter.svelte";
-
->>>>>>> Stashed changes
-
-
-
-</script>
-
-<main>
-    <div class="navbar navbar-dark bg-dark fixed-top navbar-expand-lg">
-      <div class="container">
-        <a class="navbar-brand" href="../index.html">Korea Museum</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-content">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbar-content">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item, nav-link">박물관 정보</li>
-            <li class="nav-item, nav-link">국내 유물</li>
-            <li class="nav-item, nav-link">전시회</li>
-            <li class="nav-item, nav-link">EVENT</li>
-            <li class="nav-item, nav-link">게시판</li>
-            <li class="nav-item, nav-link">Museum Shop</li>
-            <li class="nav-item, nav-link">About Us</li>
-          </ul>
-          <ul class="navbar-nav">
-            <li class="nav-item, nav-link" href="#">Log In</li>
-            <li class="nav-item, nav-link" href="#">회원가입</li>
-          </ul>
-        </div>
+  <!-- ======= Top Bar ======= -->
+  <section id="topbar" class="d-flex align-items-center">
+    <div class="container d-flex justify-content-center justify-content-md-between">
+      <div class="contact-info d-flex align-items-center">
+        <i class="bi bi-envelope d-flex align-items-center"><a href="mailto:contact@example.com"> contact@example.com</a></i>
+        <i class="bi bi-phone d-flex align-items-center ms-4"><span>+82 8852 3326</span></i>
+      </div>
+      <div class="social-links d-none d-md-flex align-items-center">
+        <a href="https://twitter.com/?lang=ko" class="twitter"><i class="bi bi-twitter"></i></a>
+        <a href="https://ko-kr.facebook.com/" class="facebook"><i class="bi bi-facebook"></i></a>
+        <a href="https://www.instagram.com/" class="instagram"><i class="bi bi-instagram"></i></a>
+        <a href="https://kr.linkedin.com/" class="linkedin"><i class="bi bi-linkedin"></i></a>
       </div>
     </div>
-  <div>
-</main>
-<p>store 활용 - 전시회 누르면 Id 가 넘어가서 원하는 골라 오기</p>
-<div id="test">
+  </section>
 
-</div>
-<<<<<<< Updated upstream
-<!-- <Exhibitiontotal /> -->
-<!-- <Relic/> -->
-<Tesr/>
-=======
->>>>>>> Stashed changes
+  <!-- ======= Header ======= -->
+  <header id="header" class="d-flex align-items-center">
+    <div class="container d-flex justify-content-between align-items-center">
+
+      <div class="logo">
+        <h1><a href="index.html">Korea Muesum</a></h1>
+        <!-- Uncomment below if you prefer to use an image logo -->
+        <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+      </div>
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="active" href="index.html">Home</a></li>
+          <li><a href="index.html">박물관소개</a></li>
+          <li><a href="index.html">유물 검색</a></li>
+          <li><a href="index.html">전시회</a></li>
+          <li><a href="index.html">EVENT</a></li>
+          <li><a href="index.html">게시판</a></li>
+          <li><a href="index.html">About Us</a></li>
+          <li><a href="cindex.html">Contact</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->
 
 
+<Upsidposter />
+
+<style>
+
+
+</style>
--- SpringProject/museum/src/lib/mainpage/Upsidposter.svelte
+++ SpringProject/museum/src/lib/mainpage/Upsidposter.svelte
@@ -6,6 +6,12 @@ let posters=[]
 let exhib=[]
 let boards=[]
 let events=[]
+let sliedtest=[
+    {number:"slide-1"},
+    {number:"slide-2"},
+    {number:"slide-3"},
+
+]
 
 const upsidebanner="http://192.168.0.51:8080/mainpage/"
 const slidingbnn="http://192.168.0.51:8080/exhibition/"
@@ -34,13 +40,57 @@ onMount(async function(){
 
 <h1>Upsideposet</h1>
 
-{#each posters as post}
-<label class="upsidebanner">
-    <button class="upsidebanner">
-        <img src = "./src/lib/mainpage/image/{post.poster}.jpg" alt="업슴">
-    </button>
-</label>
-   {/each}
+<section id="hero">
+    <div class="hero-container">
+      <div id="heroCarousel" data-bs-interval="5000" class="carousel slide carousel-fade" data-bs-ride="carousel">
+
+        <ol class="carousel-indicators" id="hero-carousel-indicators"></ol>
+
+        <div class="carousel-inner" role="listbox">
+
+          <!-- Slide 1 -->
+          <div class="carousel-item active" style="background-image: url(assets/img/slide/slide-1.jpg)">
+            <div class="carousel-container">
+              <div class="carousel-content">
+                <h2 class="animate__animated animate__fadeInDown">Welcome to <span>Eterna</span></h2>
+                <p class="animate__animated animate__fadeInUp">Ut velit est quam dolor ad a aliquid qui aliquid. Sequi ea ut et est quaerat sequi nihil ut aliquam. Occaecati alias dolorem mollitia ut. Similique ea voluptatem. Esse doloremque accusamus repellendus deleniti vel. Minus et tempore modi architecto.</p>
+                
+              </div>
+            </div>
+          </div>
+          <!-- Slide 2 -->
+          <div class="carousel-item" style="background-image: url(assets/img/slide/slide-2.jpg)">
+            <div class="carousel-container">
+              <div class="carousel-content">
+                <h2 class="animate__animated fanimate__adeInDown">Lorem <span>Ipsum Dolor</span></h2>
+                <p class="animate__animated animate__fadeInUp">Ut velit est quam dolor ad a aliquid qui aliquid. Sequi ea ut et est quaerat sequi nihil ut aliquam. Occaecati alias dolorem mollitia ut. Similique ea voluptatem. Esse doloremque accusamus repellendus deleniti vel. Minus et tempore modi architecto.</p>
+                
+              </div>
+            </div>
+          </div>
+          <!-- Slide 3 -->
+          <div class="carousel-item" style="background-image: url(assets/img/slide/slide-3.jpg)">
+            <div class="carousel-container">
+              <div class="carousel-content">
+                <h2 class="animate__animated animate__fadeInDown">Sequi ea <span>Dime Lara</span></h2>
+                <p class="animate__animated animate__fadeInUp">Ut velit est quam dolor ad a aliquid qui aliquid. Sequi ea ut et est quaerat sequi nihil ut aliquam. Occaecati alias dolorem mollitia ut. Similique ea voluptatem. Esse doloremque accusamus repellendus deleniti vel. Minus et tempore modi architecto.</p>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+        <a class="carousel-control-prev" href="#heroCarousel" role="button" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon bi bi-chevron-left" aria-hidden="true"></span>
+        </a>
+
+        <a class="carousel-control-next" href="#heroCarousel" role="button" data-bs-slide="next">
+          <span class="carousel-control-next-icon bi bi-chevron-right" aria-hidden="true"></span>
+        </a>
+
+      </div>
+    </div>
+  </section><!-- End Hero -->
 <div>
     <img src ="./src/lib/image/watch.png" alt="수정" style="width:50px">  관람시간
 <p>   월/화/목/금/일 10:00 ~ 18:00 수/토 10:00 ~ 21:00    * 입장 마감은 폐관30분 전까지</p>
@@ -86,14 +136,14 @@ if이요해서 만들기
 {/each}
 
 <style>
-.upsidebanner{
+/* .upsidebanner{
     width:100%;
     height: 300px;
 }
 .upsidebanner>img{
     width:100%;
     height : 100%;
-}
+} */
 #board{
     border: 1px solid black;
 }
