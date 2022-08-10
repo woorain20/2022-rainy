b45448c393b6b92aeaf2806dc7a024c6431ad7e3
ArcheTree <kimplp2002@naver.com>
2022-08-10 12:25:54 +0900

    control 8/10 am

--- SpringProject/museum/src/all/AppEvent.svelte
+++ SpringProject/museum/src/all/AppEvent.svelte
@@ -1,31 +1,9 @@
 <script>
-<<<<<<< Updated upstream
-    
-    import {Router, Route} from "../../node_modules/svelte-routing"
-    import Eventdetail from "../../src/lib/Eventdetail.svelte";
-=======
->>>>>>> Stashed changes
+
     import Eventpage from "../../src/lib/Eventpage.svelte";
 
 </script>
-<<<<<<< Updated upstream
+
 
 <Eventpage />
 
-<Router url={url}>
-    <!-- <nav>
-        <Link to="/">Home</Link>
-        <Link to="infomation">Infomation</Link>
-        <Link to="search">Search</Link>
-        <Link to="option">Option</Link>
-        <Link to="/">Exit</Link>
-    </nav> -->
-    <div>
-        <Route path="eventpage" component={Eventpage}><Eventpage/></Route>
-        <Route path="eventdetail" component={Eventdetail}></Route>
-        <Route path="/"></Route>
-    </div>
-</Router>
-=======
-<Eventpage/>
->>>>>>> Stashed changes
