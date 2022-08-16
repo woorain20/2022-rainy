33dde7fa88df8f5d8942507dcf13c2315bd3017f
ArcheTree <kimplp2002@naver.com>
2022-08-16 16:28:07 +0900

    test 수정중

--- SpringProject/my-app/src/lib/Test22.svelte
+++ SpringProject/my-app/src/lib/Test22.svelte
@@ -1,42 +1,42 @@
 <script>
-const slides = document.querySelector('.slides'); //전체 슬라이드 컨테이너
-const slideImg = document.querySelectorAll('.slides li'); //모든 슬라이드들
-let currentIdx = 0; //현재 슬라이드 index
-const slideCount = slideImg.length; // 슬라이드 개수
-const prev = document.querySelector('.prev'); //이전 버튼
-const next = document.querySelector('.next'); //다음 버튼
-const slideWidth = 300; //한개의 슬라이드 넓이
-const slideMargin = 100; //슬라이드간의 margin 값
-
-//전체 슬라이드 컨테이너 넓이 설정
-// slides.style.width = (slideWidth + slideMargin) * slideCount + 'px';
-
-function moveSlide(num) {
-//   slides.style.left = -num * 400 + 'px';
-  currentIdx = num;
-}
-prev.addEventListener('click',function(){
-    if (currentIdx !== 0) moveSlide(currentIdx + 1);
-})
-// prev.addEventListener('click', function () {
-//   /*첫 번째 슬라이드로 표시 됐을때는 
-//   이전 버튼 눌러도 아무런 반응 없게 하기 위해 
-//   currentIdx !==0일때만 moveSlide 함수 불러옴 */
-
-//   if (currentIdx !== 0) moveSlide(currentIdx + 1);
-// });
-
-next.addEventListener('click', function () {
-  /* 마지막 슬라이드로 표시 됐을때는 
-  다음 버튼 눌러도 아무런 반응 없게 하기 위해
-  currentIdx !==slideCount - 1 일때만 
-  moveSlide 함수 불러옴 */
-  if (currentIdx !== slideCount - 1) {
-    moveSlide(currentIdx + 1);
+let curPos = 0;
+let postion = 0;
+const IMAGE_WIDTH = 640;
+const prevBtn = document.querySelector(".prev")
+const nextBtn = document.querySelector(".next")
+const images = document.querySelector(".images")
+ 
+export function prev(){
+  if(curPos > 0){
+    nextBtn.removeAttribute("disabled")
+    postion += IMAGE_WIDTH;
+    images.style.transform = `translateX(${postion}px)`;
+    curPos = curPos - 1;
   }
-})
-// 출처: https://eunhee-programming.tistory.com/106 [코드짜는 문과녀:티스토리]
-</script>
+  if(curPos == 0){
+    prevBtn.setAttribute('disabled', 'true')
+  }
+}
+export function next(){
+  if(curPos < 3){
+    prevBtn.removeAttribute("disabled")
+    postion -= IMAGE_WIDTH;
+    images.style.transform = `translateX(${postion}px)`;
+    curPos = curPos + 1;
+  }
+  if(curPos == 3){
+    nextBtn.setAttribute('disabled', 'true')
+  }
+}
+ 
+function init(){
+  prevBtn.setAttribute('disabled', 'true')
+  prevBtn.addEventListener("click", prev)
+  nextBtn.addEventListener("click", next)
+}
+ 
+init();
+  </script>
 <h3>나오나?dddd</h3>
 
 
@@ -53,8 +53,8 @@ next.addEventListener('click', function () {
       
       <!-- &lang: 왼쪽 방향 화살표
       &rang: 오른쪽 방향 화살표 -->
-      <span class="prev">&lang;</span>  
-      <span class="next">&rang;</span>
+      <span class="prev" on:click={prev}>&lang;</span>  
+      <span class="next" on:click={next}>&rang;</span>
     </p>
   </div>
 <!-- 출처: https://eunhee-programming.tistory.com/106 [코드짜는 문과녀:티스토리] -->
