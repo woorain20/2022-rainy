1fee2e89009fa4f7bf525a141658c633452e1169
woorain20 <superwgzi@hanmail.net>
2022-10-04 11:54:01 +0900

    플랫폼 참고

--- 플랫폼 참고/파이썬 예제/BikePrepare/bike-svelte/src/lib/BikeEDA.svelte
+++ 플랫폼 참고/파이썬 예제/BikePrepare/bike-svelte/src/lib/BikeEDA.svelte
@@ -4,7 +4,15 @@
     import CardBody from "sveltestrap/src/CardBody.svelte";
     import CardHeader from "sveltestrap/src/CardHeader.svelte";
     import CardFooter from "sveltestrap/src/CardFooter.svelte";
-  
+    import year from "../assets/year.png";
+    import month from"../assets/month.png";
+    import day from"../assets/day.png";
+    import hour from"../assets/hour.png";
+    import pph from"../assets/pp-hour.png";
+    import pphs from"../assets/pp-hour-season.png";
+    import pphwe from"../assets/pp-hour-weather.png";
+    import pphwo from"../assets/pp-hour-workingday.png";
+
   </script>
   <Styles/>
   <Container>
@@ -22,7 +30,7 @@
             <h4>년도별 대여량</h4>
           </CardHeader>
           <CardBody>
-            <img src="https://via.placeholder.com/320x240" alt="year" />
+            <img src="{year}" alt="year" />
           </CardBody>
           <CardFooter class="small text-muted">
             연도별 대여량은 2011년 보다 2012년이 더 많다.
@@ -36,7 +44,7 @@
             <h4>월별 대여량</h4>
           </CardHeader>
           <CardBody>
-            <img src="https://via.placeholder.com/320x240" alt="year" />
+            <img src="{month}" alt="year" />
           </CardBody>
           <CardFooter class="small text-muted">
             월별 대여량은 6월에 가장 많고 7~10월도 대여량이 많다. 
@@ -53,7 +61,7 @@
             <h4>일별 대여량</h4>
           </CardHeader>
           <CardBody>
-            <img src="https://via.placeholder.com/320x240" alt="hour" />
+            <img src="{day}" alt="hour" />
           </CardBody>
           <CardFooter class="small text-muted">
             일별대여량은 1일부터 19일까지만 있고 나머지 날짜는 제공 되지 않았다.
@@ -66,7 +74,7 @@
             <h4>시간별 대여량</h4>
           </CardHeader>
           <CardBody>
-            <img src="https://via.placeholder.com/320x240" alt="hour" />
+            <img src="{hour}" alt="hour" />
           </CardBody>
           <CardFooter class="small text-muted">
             시간 대 대여량을 보면 출퇴근 시간에 대여량이 많은 것 같다. 
@@ -82,7 +90,7 @@
             <h4>시간별 대여량</h4>
           </CardHeader>
           <CardBody>
-            <img src="https://via.placeholder.com/320x240" alt="year" />
+            <img src="{pph}" alt="year" />
           </CardBody>
           <CardFooter class="small text-muted">
             출퇴근 시간에 대여량이 많다.
@@ -95,7 +103,7 @@
             <h4>평일 및 주말 대여량</h4>
           </CardHeader>
           <CardBody>
-            <img src="https://via.placeholder.com/320x240" alt="year" />
+            <img src="{pphwo}" alt="year" />
           </CardBody>
           <CardFooter class="small text-muted">
             평일은 출퇴근 시간에 대여량이 많고 휴일에는 오후시간이 가장 
@@ -110,7 +118,7 @@
             <h4>날씨별 대여량</h4>
           </CardHeader>
           <CardBody>
-            <img src="https://via.placeholder.com/320x240" alt="year" />
+            <img src="{pphwe}" alt="year" />
           </CardBody>
           <CardFooter class="small text-muted">
             1: 맑은 날씨 2: 안개 3: 가벼운 눈, 비 ​4: 폭설, 폭우 
@@ -124,7 +132,7 @@
             <h4>계절별 대여량</h4>
           </CardHeader>
           <CardBody>
-            <img src="https://via.placeholder.com/320x240" alt="year" />
+            <img src="{pphs}" alt="year" />
           </CardBody>
           <CardFooter class="small text-muted">
             1. 봄 2. 여름 3. 가을 4. 겨울
